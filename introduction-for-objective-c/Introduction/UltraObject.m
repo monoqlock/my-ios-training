0f10a559be173d150b047cf668632e60df2ce06f
morikawa_kaori <kaori.morikawa0507@gmail.com>
2015-01-18 18:27:09 +0900

    finish introduction-for-objective-c

--- introduction-for-objective-c/Introduction/UltraObject.m
+++ introduction-for-objective-c/Introduction/UltraObject.m
@@ -19,5 +19,20 @@
     return self;
 }
 
+- (instancetype)initWithName:(NSString *)str ultra:(NSInteger)ultra {
+    self = [self init];
+    if (self) {
+        _name = str;
+        _ultra = ultra;
+    }
+    return self;
+}
 
+- (NSString *)ultraString {
+    return [NSString stringWithFormat:@"Ultra-%@-%ld", self.name, self.ultra];
+}
+
+- (void)doubleUltra {
+    self.ultra *= 2;
+}
 @end
